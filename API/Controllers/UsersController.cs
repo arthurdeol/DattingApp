b1f5b9d499cd8959c8018f79065cc5d4c82f8dd1
Arthur de Oliveira Batista <arthurdeol@gmail.com>
2021-01-31 19:57:40 -0300

    creating controller user

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
