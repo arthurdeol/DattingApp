618579d40396e895314d4074be9f0f739a165c55
Arthur de Oliveira Batista <arthurdeol@gmail.com>
2021-02-15 18:44:36 -0300

    adding a Exception handling middleware

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,3 +1,4 @@
+using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -33,11 +34,13 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
+
             var thing = _context.Users.Find(-1);
 
             var thingToReturn = thing.ToString();
 
             return thingToReturn;
+
         }
         
         [HttpGet("bad-request")]
